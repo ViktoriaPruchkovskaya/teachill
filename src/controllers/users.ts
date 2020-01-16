79924f64f8bcb2feeed033e8e4abc0dee2c30290
Viktoria Pruchkovskaya <pruchkovskayav@gmail.com>
2020-01-16 16:06:25 +0300

    Add signup validation.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -14,14 +14,34 @@ export async function getUsers(ctx: Koa.ParameterizedContext, next: Koa.Next) {
 }
 
 export async function signupController(ctx: Koa.ParameterizedContext, next: Koa.Next) {
-  const signupService = new SignupService();
-  await signupService.doSignup(
-    ctx.request.body.username,
-    ctx.request.body.password,
-    ctx.request.body.fullName
-  );
-  ctx.body = {};
-  ctx.response.status = httpCodes.CREATED;
+  if (
+    ctx.request.body.hasOwnProperty('username') &&
+    ctx.request.body.hasOwnProperty('password') &&
+    ctx.request.body.hasOwnProperty('fullName') &&
+    ctx.request.body.username.replace(/\s/g, '').length >= 3 &&
+    ctx.request.body.password.replace(/\s/g, '').length > 0 &&
+    ctx.request.body.fullName.replace(/\s/g, '').length > 0 &&
+    ctx.request.body.username.replace(/[!()-.?_~;:#&%$*+=@]/g, '').length > 0 &&
+    /^[A-Za-z0-9!()-.?_~;:#&%$*+=@]/.test(ctx.request.body.username) &&
+    ctx.request.body.username[0] !== '.' &&
+    ctx.request.body.username[0] !== '-' &&
+    ctx.request.body.password[0] !== '.' &&
+    ctx.request.body.password[0] !== '-'
+  ) {
+    const signupService = new SignupService();
+
+    const isUsernameNotExist = await signupService.doSignup(
+      ctx.request.body.username,
+      ctx.request.body.password,
+      ctx.request.body.fullName
+    );
+    if (isUsernameNotExist) {
+      ctx.body = {};
+      ctx.response.status = httpCodes.CREATED;
+    }
+  }
+  ctx.response.status = httpCodes.BAD_REQUEST;
+
   await next();
 }
 
--- src/services/users.ts
+++ src/services/users.ts
@@ -8,10 +8,15 @@ enum RoleType {
 }
 
 export class SignupService {
-  public async doSignup(username: string, password: string, fullName: string): Promise<void> {
-    const passwordHash = await this.createPasswordHash(password);
-    const userId = await createUser(username, passwordHash, fullName);
-    await this.createUserRole(userId, RoleType.Administrator);
+  public async doSignup(username: string, password: string, fullName: string): Promise<boolean> {
+    const user = await getUserByUsername(username);
+    if (!user) {
+      const passwordHash = await this.createPasswordHash(password);
+      const userId = await createUser(username, passwordHash, fullName);
+      await this.createUserRole(userId, RoleType.Administrator);
+      return true;
+    }
+    return false;
   }
 
   private async createUserRole(userId: number, roleType: RoleType): Promise<void> {
