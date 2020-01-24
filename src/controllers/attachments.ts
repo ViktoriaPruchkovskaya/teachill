f21dd77abfe0b3ddd90709bf65a90926d33edec1
Viktoria Pruchkovskaya <pruchkovskayav@gmail.com>
2020-01-24 19:54:16 +0300

    Implement lesson attachment adding and displaying.

--- src/controllers/attachments.ts
+++ src/controllers/attachments.ts
@@ -8,6 +8,10 @@ interface AttachmentData {
   url: string;
 }
 
+interface LessonAttachmentData {
+  attachmentId: number;
+}
+
 export async function createAttachmentController(ctx: Koa.ParameterizedContext, next: Koa.Next) {
   let validatedData: AttachmentData;
   const validator = new Validator<AttachmentData>([
@@ -30,5 +34,43 @@ export async function createAttachmentController(ctx: Koa.ParameterizedContext, 
   await attachmentService.createAttachment(validatedData.name, validatedData.url);
   ctx.body = {};
   ctx.response.status = httpCodes.CREATED;
+  await next();
+}
+
+export async function createLessonAttachmentController(
+  ctx: Koa.ParameterizedContext,
+  next: Koa.Next
+) {
+  let validatedData: LessonAttachmentData;
+  const validator = new Validator<LessonAttachmentData>([
+    shouldHaveField('attachmentId', 'number'),
+  ]);
+  try {
+    validatedData = validator.validate(ctx.request.body);
+  } catch (err) {
+    if (err instanceof ValidationFailed) {
+      ctx.body = {
+        errors: err.errors,
+      };
+      ctx.response.status = httpCodes.BAD_REQUEST;
+      return await next();
+    }
+  }
+
+  const attachmentService = new AttachmentService();
+  await attachmentService.createLessonAttachment(ctx.params.lesson_id, validatedData.attachmentId);
+  ctx.body = {};
+  ctx.response.status = httpCodes.CREATED;
+  await next();
+}
+
+export async function getLessonAttachmentsController(
+  ctx: Koa.ParameterizedContext,
+  next: Koa.Next
+) {
+  const attachmentService = new AttachmentService();
+  const lessonAttachment = await attachmentService.getLessonAttachments(ctx.params.lesson_id);
+  ctx.body = { ...lessonAttachment };
+  ctx.response.status = httpCodes.OK;
   await next();
 }
--- src/services/attachments.ts
+++ src/services/attachments.ts
@@ -1,7 +1,32 @@
-import { createAttachment } from '../repositories/attachments';
+import {
+  createAttachment,
+  createLessonAttachment,
+  getLessonAttachments,
+} from '../repositories/attachments';
+
+interface LessonAttachment {
+  name: string;
+  url: string;
+}
 
 export class AttachmentService {
   public async createAttachment(name: string, url: string): Promise<void> {
     return await createAttachment(name, url);
+  }
+
+  public async createLessonAttachment(lessonId: number, attachmentId: number): Promise<void> {
+    return await createLessonAttachment(lessonId, attachmentId);
+  }
+
+  public async getLessonAttachments(lessonId: number): Promise<LessonAttachment[]> {
+    const res = getLessonAttachments(lessonId);
+    const lessonAttachment: LessonAttachment[] = (await res).map(attachment => {
+      const res: LessonAttachment = {
+        name: attachment.name as string,
+        url: attachment.url as string,
+      };
+      return res;
+    });
+    return lessonAttachment;
   }
 }
